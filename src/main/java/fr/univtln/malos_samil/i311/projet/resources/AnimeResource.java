45d12439108d10570979076848a009525037031f
SimonAmil1 <simon-almicaro@etud.univ-tln.fr>
2019-01-28 02:04:58 +0100

    ajoute un service web (REST) + modifie des propriétés graphiques JSF

---
+++ src/main/java/fr/univtln/malos_samil/i311/projet/resources/AnimeResource.java
@@ -0,0 +1,55 @@
+package fr.univtln.malos_samil.i311.projet.resources;
+
+
+import fr.univtln.malos_samil.i311.projet.jpa.anime.Anime;
+import fr.univtln.malos_samil.i311.projet.jpa.anime.AnimeCrud;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Stateless
+@Path("/anime")
+@Produces({"application/json","application/xml"})
+@Consumes({"application/json","application/xml"})
+public class AnimeResource {
+
+    @Inject
+    AnimeCrud animeCrud;
+
+    @GET
+    @Produces("text/plain")
+    public String getIt() {
+        return "Hi there!";
+    }
+
+    @GET
+    @Path("/animes/{id}")
+    @Produces("application/json")
+    public Anime getAnime(@PathParam("id") final String id){
+        return animeCrud.getAnime(id);
+    }
+
+    @GET
+    @Path("/animes")
+    @Produces("application/json")
+    public List<Anime> getAll(){
+        return animeCrud.getAll(1,3,"Boku");
+    }
+
+    @POST
+    @Path("/animes")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addAnime(Anime anime){
+        animeCrud.addAnime(anime);
+    }
+
+    @PUT
+    @Path("/animes")
+    public void updateAnime(Anime anime){
+        animeCrud.updateAnime(anime);
+    }
+
+}
